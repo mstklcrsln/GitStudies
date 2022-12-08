7e08086f7e345dead0fa76cef4b858997ae5f187
mustafa Baz <mustafabaz861@gmail.com>
2022-12-08 20:05:58 +0300

    Update Example.java

--- src/group8GitClass/Example.java
+++ src/group8GitClass/Example.java
@@ -2,6 +2,11 @@ package group8GitClass;
 
 public class Example {
     public static void main(String[] args) {
+        
+        int a=3;
+        int b=5;
+         int total=a+b;
+        system.out.println(total);
 
         //This is an example for git
     }
