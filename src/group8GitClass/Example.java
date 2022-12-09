889b82a9f041053f3f5f42d84be5f8270c47cefa
zulalll <zkutluer10@gmail.com>
2022-12-09 12:41:14 +0300

    First commit

--- src/group8GitClass/Example.java
+++ src/group8GitClass/Example.java
@@ -4,5 +4,11 @@ public class Example {
     public static void main(String[] args) {
 
         //This is an example for git
+
+        //Let's write some codes
+
+        //Let's see if they are working or not.
+
+        //There will be output or not.
     }
 }
