d3b978cb8e83e6268981569f26ffb275ff74198d
vahap iren <vahapiren@gmail.com>
2022-12-10 23:44:35 +0300

    added 123

--- src/group8GitClass/Testers.java
+++ src/group8GitClass/Testers.java
@@ -4,6 +4,7 @@ public class Testers {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
         System.out.println("Something wrong");
+        System.out.println("Why we arent study?");
 
     }
 }
