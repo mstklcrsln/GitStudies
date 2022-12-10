ba2ee73206f336ceb0d2c9795ef75797172cdad2
vahap iren <vahapiren@gmail.com>
2022-12-10 23:45:27 +0300

    study request

--- src/group8GitClass/Testers.java
+++ src/group8GitClass/Testers.java
@@ -4,7 +4,7 @@ public class Testers {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
         System.out.println("Something wrong");
-        System.out.println("Why we arent study?");
+        System.out.println("Why we arent study!!!?");
 
     }
 }
